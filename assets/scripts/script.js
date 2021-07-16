071ab9481d62a457f822a8e1ae84af579c0cef73
Ray Ashir <rayeed.ashir@gmail.com>
2021-07-15 17:04:19 -0700

    added header to the index page

---
+++ assets/scripts/script.js
@@ -0,0 +1,24 @@
+/*
+todo: 
+1. create the landing no search page
+    1.1 optional show current location weather
+    1.2 optional add suggestion to cities
+2. Search for a city 
+3. get weather for city being searched
+4. add weather to display 
+    4.1 add display date and current state of the weather icon (sunny, cloudy, etc)
+    4.2 add wind speed display 
+    4.3 add humidity display
+    4.4 add uv index display 
+    4.5 add uv index display colore code
+    4.6 add 5 day forcast
+    4.6 optional addd toggle for C/F 
+5. add search history 
+    5.1 implement ability to click button and populate the weather data
+    5.2 optional: add remove history city option
+6. create CSS 
+    6.1 add top header
+    6.2 add search area
+    6.3 add 5 day forcast
+    6.4 add 1 day forcast
+*/
