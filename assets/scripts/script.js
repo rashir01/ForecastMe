2834c9111cfadf739d70c9500e612e53579a1ebd
Ray Ashir <rayeed.ashir@gmail.com>
2021-07-15 18:03:51 -0700

    update the search area to have placeholder text

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,15 @@
 /*
 todo: 
-1. create the landing no search page
+1. create the landing and search page
     1.1 optional show current location weather
     1.2 optional add suggestion to cities
+    1.3 add header --DONE
+    1.4 optional add footer 
+    1.5 add search field 
+        1.5.1 suggestions
+        1.5.2 set search field default text 
+    1.6 add search button
+
 2. Search for a city 
 3. get weather for city being searched
 4. add weather to display 
@@ -16,8 +23,11 @@ todo:
 5. add search history 
     5.1 implement ability to click button and populate the weather data
     5.2 optional: add remove history city option
+    5.3 save searched cities
+        5.3.1 ignore invalid cities
+        5.3.2 persist saved cities
 6. create CSS 
-    6.1 add top header
+    6.1 add top header //DONE
     6.2 add search area
     6.3 add 5 day forcast
     6.4 add 1 day forcast
