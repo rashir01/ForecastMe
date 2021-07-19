4a8b8f8d0f09bd75b226f8e035b37d678e719e68
Ray Ashir <rayeed.ashir@gmail.com>
2021-07-18 22:23:36 -0700

    refactored code to get coordinates in order to have the full data with uvindex

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -37,28 +37,111 @@ todo:
         6.4.1 remove ul bullets
 */
 
-const currentDayDisplayDiv = $("")
-var currentCity = "Seattle";
-
+var currentCity = "";
+const API_KEY = "7f2a300c44e460bbccdcac14620347c7";
 
 function handleSearchSubmitButton() {
     let city = $("#city-name-text-area").val().trim();
-    getFullWeatherDataFromAPI(city);
-    addCityToSearchHistory(city);
+    processRequest(city);
 }
 
+function processRequest(city) {
+    city = "Seattle";
+    console.log("welcome to process request " + city )
+    const COORD_API_URL = "https://api.openweathermap.org/data/2.5/weather?q=" +city +  "&appid=" + API_KEY;
+    fetch(COORD_API_URL).then(function(response) {
+        return response.json();
+    }).then (getFullWeatherData)
+    .then (printWeatherData);
+    /*
+        1. get coordinates
+        2. get full week data
+        3. print 1 day 
+        4. print uv 
+        5. print 5 day
+        6. add to history
+    */
+}
+
+function printWeatherData(fullWeatherData) {
+    console.log(fullWeatherData);
+}
+function getFullWeatherData(data) {
+    const FULL_WEATHER_URL = `https://api.openweathermap.org/data/2.5/onecall?lon=${data.coord.lon}&lat=${data.coord.lat}&exclude=hourly,minutely,alerts&appid=${API_KEY}&units=imperial`;
+    console.log(`inside of fullWeather data and api is ${FULL_WEATHER_URL}`)
+    console.log(data);
+    fetch(FULL_WEATHER_URL).then(function(response) {
+        return response.json();
+    }).then(printWeatherData)
+}
+/*function handleAPICalls(city) {
+    const COORD_API_URL = "https://api.openweathermap.org/data/2.5/weather?q=" +city +  "&appid=" + API_KEY;
+    const FULL_WEATHER_URL = 
+    fetch(COORD_API_URL).then(function(response) {
+        return response.json();
+    }).then (function (coordiatesJSON) {
+        const FULL_WEATHER_URL = `https://api.openweathermap.org/data/2.5/onecall?lon=${coordiatesJSON.coord.lon}&lat=${coordiatesJSON.coord.lat}&exclude=hourly,minutely,alerts&appid=${API_KEY}&units=imperial`;
+        fetch(FULL_WEATHER_URL).then(function(fullWeatherDataRaw) {
+            return fullWeatherDataRaw.json();
+        })
+        return coordiatesJSON;
+    })
+
+
+
+
+
+
+    .then (printCurrentDayTemps).
+    then(getUVI);
+}
+
+function printCurrentDayTemps(data) {
+    //let temp = data.main.temp;
+    //get current icon
+    //let icon = data.weather[0].icon; 
+    //get current humidity
+    //let humidity = data.main.humidity;
+    //get current wind speed
+    //let windspeed = data.wind.speed;
+    //get uvi
+    // getUVI(data).then(function() {
+    //     console.log (`temp ${temp} icon ${icon} humidity ${humidity} windspeed ${windspeed}`);
+    // });
+        
+        console.log(`this is current day temps ${data}`);
+    
+    return data;
+}
+
+function getUVI(data) {
+    // let apiQuery = `https://api.openweathermap.org/data/2.5/onecall?lon=${data.coord.lon}&lat=${data.coord.lat}&exclude=hourly,minutely,alerts&appid=${API_KEY}&units=imperial`;
+    // fetch(apiQuery).then(function(response) {
+    //     return response.json();
+    // })
+    // .then(function(data) {
+         console.log(`uvi ${data}`);
+         return data;
+    // });
+
+}
+
+//use the current api to get long/lat
+//use long/lat to do onecall
+//display temps from onecall
+
+
+/*
 function buildQueryURL(city) {
     const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
     const API_FIVE_DAY_URL = "https://api.openweathermap.org/data/2.5/forecast?q=";
-    const API_ONE_CALL =  "https://api.openweathermap.org/data/2.5/onecall?lon=-122.335167&lat=47.608013&exclude=hourly,minutely"
-    const API_KEY = "7f2a300c44e460bbccdcac14620347c7";
-    const APPID_QUERY_PARAM = "&appid=";
+    const API_ONE_CALL =  "https://api.openweathermap.org/data/2.5/onecall?lon=-122.335167&lat=47.608013&exclude=hourly,minutely,alerts"
+    const APPID_QUERY_PARAM = "&appid=" + API_KEY;
     const TEMP_QUERY_PARAM = "&units=imperial";
-    return API_ONE_CALL +  APPID_QUERY_PARAM + API_KEY + TEMP_QUERY_PARAM ;
+    return API_ONE_CALL +  APPID_QUERY_PARAM  + TEMP_QUERY_PARAM ;
 }
-function getCityLongLat(city) {
 
-}
+
 function addCityToSearchHistory(city) {}
 function getFullWeatherDataFromAPI(city) {
     fetch(buildQueryURL(city))
@@ -98,7 +181,7 @@ function populateCurrentDayData(data){
     //
     setUVIndexBackground(data.current.uvi);
 }
-
+*/
 
 
 
